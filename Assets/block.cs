49b73011a25ea421468beab05ed719b43e1ce2f9
kt1672341 <takeda6726@gmail.com>
2018-07-23 21:43:21 +0900

    GoForwardUnityChanの課題
    課題を作成しました、ご確認お願い致します。

---
+++ Assets/block.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class block : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter2D(Collision2D other) {
+
+        //オブジェクトに衝突した場合
+        if (other.gameObject.tag == "blockTag") {
+
+
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
+
+
